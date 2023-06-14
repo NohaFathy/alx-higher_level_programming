5f4c925bf0cb5cdc48eeec36d5b566b49583c908
Noha Fathy <nohazaid97@gmail.com>
2023-06-14 15:11:03 -0700

    -

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[j**2 for j in i ]for i in matrix]
-    return (new_ matrix)
+def search_replace(my_list, search, replace):
+     return([replace if number == search else number for number in my_list])
